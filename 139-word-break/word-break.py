7d496ff918932ccec3a53cd3f243527fd8deaf94
Sidra Ahmad <54542582+sidrahmad@users.noreply.github.com>
2025-11-18 18:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.35%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,12 +1,16 @@
 class Solution:
     def wordBreak(self, s, wordDict): 
-        dp = [False]*(len(s)+1)
-        dp[0] = True
-
-        for i in range(1,len(s)+1):
-            for j in range(i):
-                if dp[j] and s[j:i] in wordDict:
+        n = len(s)
+        dp = [False] * n
+        for i in range(n):
+            for word in wordDict:
+                # check if is the end index of the word
+                lw = len(word)
+                if i < lw -1:
+                    continue
+                substr = s[i - lw + 1 : i + 1]
+                if word == substr and (i - lw < 0 or dp[i - lw] == True):
                     dp[i] = True
                     break
-        
-        return dp[-1]
+
+        return dp[n-1]
