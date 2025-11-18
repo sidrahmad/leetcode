6793f5442fcd7e45a6515d1b18acc8c55339d820
Sidra Ahmad <54542582+sidrahmad@users.noreply.github.com>
2025-11-18 20:24:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (99.03%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,22 +1,17 @@
-from typing import List
-from functools import lru_cache
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        word_set = set(wordDict)
-        n = len(s)
-        max_len = max((len(w) for w in word_set), default=0)
+      
+        ans = []
+        wordSet = set(wordDict)  # Convert list to set for efficient lookup
 
-        @lru_cache(maxsize=None)
-        def dfs(start: int) -> List[str]:
-            if start == n:
-                return [""]
-            res = []
-            for end in range(start + 1, min(n, start + max_len) + 1):
-                word = s[start:end]
-                if word in word_set:
-                    for tail in dfs(end):
-                        res.append(word if not tail else word + " " + tail)
-            return res
+        def dfs(pref: str, suff: str) -> None:
+            if suff == '':
+                ans.append(pref.rstrip())  # Add the valid sentence to the answer list
+            else:
+                for i in range(len(suff)):
+                    if suff[:i+1] not in wordSet: 
+                        continue
+                    dfs(pref + suff[:i+1] + ' ', suff[i+1:])  # Fix slicing issue
 
-        return dfs(0)
+        dfs('', s)
+        return ans
